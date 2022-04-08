2cf432ee1c432e183d8d9002a8af8782663611c7
Felipe Borges <lipex360@gmail.com>
2022-04-08 18:12:40 -0300

    Ribbon Component Styles updated

--- src/components/Ribbon/styles.ts
+++ src/components/Ribbon/styles.ts
@@ -1,32 +1,66 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import { RibbonColors, RibbonProps } from '.'
+import { darken } from 'polished'
 // import media from 'styled-media-query'
 
 const wrapperModifiers = {
   color: (theme: DefaultTheme, color: RibbonColors) => css`
     background-color: ${theme.colors[color]};
+
+    &::after {
+      border-left-color: ${darken(0.2, theme.colors[color])};
+      border-top-color: ${darken(0.2, theme.colors[color])};
+    }
   `,
 
   normal: (theme: DefaultTheme) => css`
-    height: 3.6rem;
     font-size: ${theme.font.sizes.small};
+    padding: 0 ${theme.spacings.small};
+    height: 3.6rem;
+    right: -2rem;
+
+    &::after {
+      top: 3.6rem;
+      border-top-width: 1rem;
+      border-right-width: 2rem;
+    }
   `,
 
   small: (theme: DefaultTheme) => css`
-    height: 2.6rem;
     font-size: ${theme.font.sizes.xsmall};
+    padding: 0 ${theme.spacings.xsmall};
+    height: 2.6rem;
+    right: -1.5rem;
+
+    &::after {
+      top: 2.6rem;
+      border-top-width: 0.7rem;
+      border-right-width: 1.5rem;
+    }
   `
 }
 
 export const Wrapper = styled.div<RibbonProps>`
   ${({ theme, color, size }) => css`
-    ${!!size && wrapperModifiers[size](theme)}
-    ${!!color && wrapperModifiers.color(theme, color)}
-  `}
-`
+    position: absolute;
+    top: ${theme.spacings.xsmall};
+    display: flex;
+    align-items: center;
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.white};
 
-export const Title = styled.h2`
-  ${({ theme }) => css`
-    color: ${theme.colors.black};
+    &::after {
+      content: '';
+      position: absolute;
+      right: 0;
+      border-style: solid;
+      border-left-width: 0;
+      border-right-color: transparent;
+      border-bottom-color: transparent;
+      border-bottom-width: 1rem;
+    }
+
+    ${!!size && wrapperModifiers[size](theme)}
+    ${!!color && wrapperModifiers.color(theme, color)};
   `}
 `
