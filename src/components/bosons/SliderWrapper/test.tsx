9295aa6ce3ec559e10d3f6ee2d6a893ab47df298
Felipe Borges <lipex360@gmail.com>
2022-04-12 22:38:13 -0300

    Slider Component tests added

--- src/components/bosons/SliderWrapper/test.tsx
+++ src/components/bosons/SliderWrapper/test.tsx
@@ -1,20 +1,34 @@
+import 'match-media-mock'
+
 import { screen } from '@testing-library/react'
+import { Settings } from 'react-slick'
 import { renderWithTheme } from 'utils/helpers/tests'
 
-import SliderWrapper, { SliderWrapperProps } from '.'
+import SliderWrapper from '.'
 
-const props = {
-  title: 'SliderWrapper'
-} as SliderWrapperProps
+const horizontalSettings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+} as Settings
 
 describe('<SliderWrapper />', () => {
-  it('should render the heading', () => {
-    const { debug, container } = renderWithTheme(<SliderWrapper {...props} />)
+  it('should render children as slider item', () => {
+    const { container } = renderWithTheme(
+      <SliderWrapper settings={horizontalSettings}>
+        <p>item 1</p>
+        <p>item 2</p>
+      </SliderWrapper>
+    )
 
-    const sliderwrapper = screen.getByRole('heading', { name: /SliderWrapper/i })
+    const slide1 = screen.getByText(/item 1/i)
+    const slide2 = screen.getByText(/item 2/i)
 
-    debug(container)
+    expect(slide1.parentElement?.parentElement).toHaveClass('slick-slide')
+    expect(slide2.parentElement?.parentElement).toHaveClass('slick-slide')
 
-    expect(sliderwrapper).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
