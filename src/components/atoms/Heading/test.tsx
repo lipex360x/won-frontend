b626149d6afd3157b34faa1ea4f2d5f8fc4d769b
Felipe Borges <lipex360@gmail.com>
2022-04-27 22:55:49 -0300

    Heading component huge size added

--- src/components/atoms/Heading/test.tsx
+++ src/components/atoms/Heading/test.tsx
@@ -7,49 +7,47 @@ describe('<Heading />', () => {
   it('should render a black heading by default', () => {
     renderWithTheme(<Heading>Won Games</Heading>)
 
-    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      color: '#030517'
-    })
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({ color: '#030517' })
   })
 
   it('should render a black heading when color is passed', () => {
     renderWithTheme(<Heading color="black">Won Games</Heading>)
 
-    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      color: '#030517'
-    })
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({ color: '#030517' })
   })
 
   it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
 
-    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #F231A5'
-    })
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
   })
 
   it('should render a heading with a line at the bottom', () => {
     renderWithTheme(<Heading lineBottom>Won Games</Heading>)
 
-    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
-      'border-bottom',
-      '0.5rem solid #F231A5',
-      { modifier: '::after' }
-    )
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
   })
 
   it('should render a heading with a small size', () => {
     renderWithTheme(<Heading size="small">Won Games</Heading>)
 
-    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'font-size': '1.6rem'
-    })
+    const heading = screen.getByRole('heading', { name: /won games/i })
 
-    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
-      'width',
-      '3rem',
-      { modifier: '::after' }
-    )
+    expect(heading).toHaveStyle({ 'font-size': '1.6rem' })
+    expect(heading).toHaveStyleRule('width', '3rem', { modifier: '::after' })
+  })
+
+  it('should render a heading with a huge size', () => {
+    renderWithTheme(<Heading size="huge">Won Games</Heading>)
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({ 'font-size': '5.2rem' })
   })
 
   it('should render a heading with a primary line color', () => {
